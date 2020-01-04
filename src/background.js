531b65f690a880bc5114867bf80a4c6909f0a3e0
Christos Papageorgiou <root.expert.xda@gmail.com>
2020-01-04 19:42:35 +0200

    moniteur-admin: Add logging.

--- src/background.js
+++ src/background.js
@@ -8,6 +8,9 @@ const { autoUpdater } = require("electron-updater");
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+autoUpdater.logger = require("electron-log")
+autoUpdater.logger.transports.file.level = "info"
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
